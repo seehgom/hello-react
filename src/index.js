68c8388f2c179b6ed828a2f42df7d539dead8358
seehgom <the.seehgom@gmail.com>
2022-04-12 11:09:11 -0400

    Creating a sample app to test on netlify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Checkbox = () => {
+  const [checked, toggle] = useReducer((val) => !val, false);
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      {checked ? "checked" : "unchecked"}
+    </>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return <Checkbox />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
